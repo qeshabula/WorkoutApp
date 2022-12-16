05cf429ac147efedc1fa548de6a03647c488f047
Bula <umbetalyevv@gmail.com>
2022-12-16 18:33:14 +0600

    Added stepsCounter

--- WorkoutApp/Controllers/Session/SessionController.swift
+++ WorkoutApp/Controllers/Session/SessionController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 final class SessionController: WABaseController {
     private let timerView = TimerView()
+    private let statsView = StatsView(with: R.Strings.Session.workoutStats)
+    private let stepsView = StepsView(with: R.Strings.Session.stepsCounter)
 
     private let timerDuration = 5.0
 
@@ -40,6 +42,8 @@ extension SessionController {
         super.setupViews()
 
         view.setupView(timerView)
+        view.setupView(statsView)
+        view.setupView(stepsView)
     }
 
     override func constraintViews() {
@@ -48,7 +52,16 @@ extension SessionController {
         NSLayoutConstraint.activate([
             timerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
             timerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 15),
-            timerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15)
+            timerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+
+            statsView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 15),
+            statsView.topAnchor.constraint(equalTo: timerView.bottomAnchor, constant: 10),
+            statsView.trailingAnchor.constraint(equalTo: view.centerXAnchor, constant: -7.5),
+
+            stepsView.leadingAnchor.constraint(equalTo: view.centerXAnchor, constant: 7.5),
+            stepsView.topAnchor.constraint(equalTo: statsView.topAnchor),
+            stepsView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
+            stepsView.heightAnchor.constraint(equalTo: statsView.heightAnchor),
         ])
     }
 
@@ -65,6 +78,16 @@ extension SessionController {
         timerView.callBack = { [weak self] in
             self?.navBarRightButtonHandler()
         }
+
+        statsView.configure(with: [.heartRate(value: "155"),
+                                   .averagePace(value: "8'20''"),
+                                   .totalSteps(value: "7,682"),
+                                   .totalDistance(value: "8.25")])
+
+        stepsView.configure(with: [.init(value: "8k", heightMultiplier: 1, title: "2/14"),
+                                   .init(value: "7k", heightMultiplier: 0.8, title: "2/15"),
+                                   .init(value: "5k", heightMultiplier: 0.6, title: "2/16"),
+                                   .init(value: "6k", heightMultiplier: 0.3, title: "2/17")])
     }
 }
 
---
+++ WorkoutApp/Controllers/Session/Views/Stats/StatsView.swift
@@ -0,0 +1,50 @@
+//
+//  StatsView.swift
+//  WorkoutApp
+//
+//  Created by Bula on 11/9/22.
+//
+
+import UIKit
+
+final class StatsView: WABaseInfoView {
+
+    private let stackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.spacing = 15
+        return view
+    }()
+
+    func configure(with itmes: [StatsItem]) {
+        itmes.forEach {
+            let itmeView = StatsItemView()
+            itmeView.configure(with: $0)
+            stackView.addArrangedSubview(itmeView)
+        }
+    }
+}
+
+extension StatsView {
+    override func setupViews() {
+        super.setupViews()
+
+        setupView(stackView)
+    }
+
+    override func constaintViews() {
+        super.constaintViews()
+
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15),
+        ])
+    }
+
+    override func configureAppearance() {
+        super.configureAppearance()
+    }
+}
+
